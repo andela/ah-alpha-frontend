d36c2943c0adfe74b1ef8935298ec4fb2a21b315
Erastus Ruiru <erastusruiru@Erastuss-MacBook-Pro.local>
2019-02-06 14:51:11 +0300

    ch(setup): redux setup - Create the appropriate folder/file structure for React/Redux workflow
    [Delivers #162948865]

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // Class component
 import Home from "./components/Home";
 import Login from "./components/Login";
+import store from "./redux/store";
 import "./App.css";
 
 // Functional component
 import Navbar from "./components/Navbar";
 
 const App = () => (
-  <Router>
-    <div className="App">
-      <Navbar />
-      <div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-        </Switch>
+  <Provider store={store}>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </div>
       </div>
-    </div>
-  </Router>
+    </Router>
+  </Provider>
 );
 
 export default App;
